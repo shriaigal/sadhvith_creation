29941d9a7eb8c1bf0a1bab85fcdf3278653396a4
A S Shridatta Aigal <shriaigal@gmail.com>
2026-09-12 21:02:27 +0530

    your changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,49 +27,68 @@ export default function App() {
 
         <main>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/products/:slug" element={<ProductDetails />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
+  <Route path="/" element={<Home />} />
 
-            <Route path="/manager/login" element={<ManagerLogin />} />
-            <Route path="*" element={<ManagerRegister />} />
-            <Route
-              path="/manager/dashboard"
-              element={
-                <ProtectedRoute>
-                  <ManagerDashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/manager/products"
-              element={
-                <ProtectedRoute>
-                  <ManageProducts />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/manager/products/add"
-              element={
-                <ProtectedRoute>
-                  <AddProduct />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/manager/products/edit/:id"
-              element={
-                <ProtectedRoute>
-                  <EditProduct />
-                </ProtectedRoute>
-              }
-            />
+  <Route path="/products" element={<Products />} />
+  <Route path="/products/:slug" element={<ProductDetails />} />
+  <Route path="/about" element={<About />} />
+  <Route path="/contact" element={<Contact />} />
 
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+  {/* Manager Authentication */}
+  <Route
+    path="/manager/login"
+    element={<ManagerLogin />}
+  />
+
+  <Route
+    path="/manager/register"
+    element={<ManagerRegister />}
+  />
+
+  {/* Manager Dashboard */}
+  <Route
+    path="/manager/dashboard"
+    element={
+      <ProtectedRoute>
+        <ManagerDashboard />
+      </ProtectedRoute>
+    }
+  />
+
+  {/* Manager Products */}
+  <Route
+    path="/manager/products"
+    element={
+      <ProtectedRoute>
+        <ManageProducts />
+      </ProtectedRoute>
+    }
+  />
+
+  <Route
+    path="/manager/products/add"
+    element={
+      <ProtectedRoute>
+        <AddProduct />
+      </ProtectedRoute>
+    }
+  />
+
+  <Route
+    path="/manager/products/edit/:id"
+    element={
+      <ProtectedRoute>
+        <EditProduct />
+      </ProtectedRoute>
+    }
+  />
+
+  {/* 404 */}
+  <Route
+    path="*"
+    element={<NotFound />}
+  />
+</Routes>
         </main>
 
         <Footer />
